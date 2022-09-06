3481440354ee1fa81daf745a2d013b1340c22e61
Claudiu Neamtu <claudiu.neamtu.31@gmail.com>
2022-09-06 22:49:25 +0300

    update devices list design

--- src/screens/DevicesListScreen.tsx
+++ src/screens/DevicesListScreen.tsx
@@ -1,44 +1,42 @@
-import React, { useCallback } from "react";
-import { FlatList, ListRenderItemInfo, StyleSheet, TouchableOpacity, View } from "react-native";
-import { useSelector } from "react-redux";
-import { Device } from "react-native-ble-plx";
-import PBText from "../components/PBText";
-import BluetoothManager from "../bluetooth/BluetoothManager";
-import { selectAllDevices } from "store/Bluetooth/selectors";
-import { useScreenHeader } from "navigation/hooks";
-import { MDevice } from "store/Bluetooth/slice";
-import { Divider, Text } from "react-native-paper";
-
-
-const DeviceRow = ({ item: device }: ListRenderItemInfo<MDevice>) => {
-    const onPress = useCallback(() => {
-        console.log("connecting ...")
-        BluetoothManager.connectToDevice(device.id);
-    }, [device]);
-
-    return (
-        <TouchableOpacity style={styles.row} onPress={onPress}>
-            <Text style={styles.deviceName}>
-                {device.name}
-            </Text>
-            <Text style={styles.deviceMac}>
-                {device.id}
-            </Text>
-        </TouchableOpacity>
-    );
-};
+import React, { useCallback } from 'react';
+import { FlatList, ListRenderItemInfo, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+import { State } from 'react-native-ble-plx';
+import { selectAllDevices, selectIsConnected } from 'store/Bluetooth/selectors';
+import { useScreenHeader } from 'navigation/hooks';
+import { MDevice } from 'store/Bluetooth/slice';
+import { Divider } from 'react-native-paper';
+import { useBluetoothState } from 'bluetooth/utils';
+import BluetoothDisabled from 'components/Devices/BluetoothDisabled';
+import DeviceListItem from 'components/Devices/DeviceListItem';
+import { useAppSelector } from 'store';
 
 const DevicesListScreen = () => {
     useScreenHeader({
-        headerTitle: "Devices"
-    })
+        headerTitle: 'Devices',
+    });
 
     const devices = useSelector(selectAllDevices);
+    const bluetoothState = useBluetoothState();
+
+    const deviceConnected = useAppSelector(selectIsConnected);
+
+    const renderItem = useCallback(
+        ({ item }: ListRenderItemInfo<MDevice>) => {
+            return <DeviceListItem device={item} connected={deviceConnected} />;
+        },
+        [deviceConnected],
+    );
+
+    if (bluetoothState !== State.PoweredOn) {
+        return <BluetoothDisabled />;
+    }
 
     return (
         <FlatList
             data={devices}
-            renderItem={(props: ListRenderItemInfo<MDevice>) => <DeviceRow {...props} />}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
             contentContainerStyle={styles.contentContainer}
             ItemSeparatorComponent={Divider}
         />
@@ -46,20 +44,11 @@ const DevicesListScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    contentContainer: {
-        padding: "5%",
-    },
+    contentContainer: {},
     row: {
         paddingVertical: 10,
         borderBottomWidth: 1,
-        borderBottomColor: "black",
-    },
-    deviceName: {
-        fontSize: 16,
-    },
-    deviceMac: {
-        marginTop: 6,
-        color: "gray",
+        borderBottomColor: 'black',
     },
 });
 
